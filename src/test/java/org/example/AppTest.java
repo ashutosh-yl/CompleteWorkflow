24d9c2673bceec5167bbcfecc025cc5a4e39df10
ashutoshyl <ashutoshraghuwanshi0001@gmail.com>
2025-07-24 21:35:23 +0530

    updated compose

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,30 +1,32 @@
 package org.example;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class AppTest {
+import java.net.URL;
 
+public class AppTest {
     WebDriver driver;
 
     @Test
     public void titleCheck() {
-
         try {
             ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless=new"); // New headless mode (recommended)
-            options.addArguments("--no-sandbox"); // Required in CI
-            options.addArguments("--disable-dev-shm-usage"); // Prevent crashes in limited environments
+            options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
 
-            driver = new ChromeDriver(options);
+            // Connect to selenium/standalone-chrome container
+            driver = new RemoteWebDriver(new URL("http://selenium:4444/wd/hub"), options);
+
             driver.get("https://www.google.com");
-
             Assert.assertEquals(driver.getTitle(), "Google");
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
-            if (driver != null) driver.quit();
+            if (driver != null)
+                driver.quit();
         }
     }
 }
